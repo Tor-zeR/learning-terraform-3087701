7617c9f6d2f6a968c95227baedb9e7baf810b05a
Tor-zeR <143712049+Tor-zeR@users.noreply.github.com>
2023-09-04 19:40:24 +0300

    Additonal fixes

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 data "aws_ami" "app_ami" {
+  most_recent = true
+
   filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
@@ -34,7 +36,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog.security_group.id]
+  vpc_security_group_ids = [module.blog.security_group_id]  
 
   tags = {
     Name = "EC2 TF Instance"
@@ -48,15 +50,9 @@ module "blog" {
 
   vpc_id              = module.vpc.public_subnets[0]
 
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp" , "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules        = [
-    {
-      protocol = "tcp"
-      port_range = "80-443"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+  egress_rules        = [ "all-all" ]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
