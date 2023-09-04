ad344dd908b35d7613540b8ee467af4c32174394
Tor-zeR <143712049+Tor-zeR@users.noreply.github.com>
2023-09-04 15:44:36 +0300

    Added SG as module

--- main.tf
+++ main.tf
@@ -22,11 +22,25 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]  # Use the security group ID here
+  vpc_security_group_ids = [module.blog_sg.security_group_id]  
 
   tags = {
     Name = "EC2 TF Instance"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new_tfmodule"
+
+  vpc_id              = data.aws_vpc.default_tf.id
+
+  ingress_rules       = ["http-80-tcp" , "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules        = [ "all-all" ]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
