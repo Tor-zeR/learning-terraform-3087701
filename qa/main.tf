ce2ce5222832fb8a8e1105106d8f121eb6783f59
Tor-zeR <143712049+Tor-zeR@users.noreply.github.com>
2023-09-06 12:30:15 +0300

    Define QA env2

--- qa/main.tf
+++ qa/main.tf
@@ -3,11 +3,9 @@ module "qa" {
 
     environment = {
         name           = "qa"
-        network_prefix = "10.1"
-
+        network_prefix = "10.2"
     }
 
     asg_min_size = 1
     asg_max_size = 1
-
 }
